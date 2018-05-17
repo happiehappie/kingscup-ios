e05f1425295d8c071d936d3539b3a9fa08fa97fa
Stanley Lim <stanleylim@Jacks-MacBook-Pro-2.local>
2018-05-18 01:48:59 +0800

    Code refactor for quick guide and constraint

--- kingscup/QuickGuide/QuickGuideViewController.swift
+++ kingscup/QuickGuide/QuickGuideViewController.swift
@@ -28,24 +28,18 @@ class QuickGuideViewController: UIViewController {
     }
     
     func setup() {
+        self.quickGuideStackView.addArrangedSubview(self.createLabel(with: "Yes, this is a drinking game.\nDoesn't have to be alcoholic.\n\nEveryone have your own\ndrinking cup and place a cup in\nthe middle to be the King's Cup."))
+        self.quickGuideStackView.addArrangedSubview(self.createLabel(with: "Pick a person to start a game,\nselect a card and do what it says.\n\nThere is no escape!\nPass to the next person when you\nare done."))
+        self.quickGuideStackView.addArrangedSubview(self.createLabel(with: "Draw a King from the deck,\nyou can choose any drinks \npour into the King's Cup.\n\nPerson with the last King will\nhave to finish the cup in one shot!\nCheers mate."))
+    }
+    
+    func createLabel(with text: String) -> UILabel {
         let label = UILabel()
-        label.text = "Yes, this is a drinking game.\nDoesn't have to be alcoholic.\n\nEveryone have your own\ndrinking cup and place a cup in\nthe middle to be the King's Cup."
+        label.text = text
         label.numberOfLines = 0
         label.textAlignment = .center
         label.textColor = UIColor.white
-        let label2 = UILabel()
-        label2.text = "Pick a person to start a game,\nselect a card and do what it says.\n\nThere is no escape!\nPass to the next person when you\nare done."
-        label2.numberOfLines = 0
-        label2.textAlignment = .center
-        label2.textColor = UIColor.white
-        let label3 = UILabel()
-        label3.text = "Draw a King from the deck,\nyou can choose any drinks \npour into the King's Cup.\n\nPerson with the last King will\nhave to finish the cup in one shot!\nCheers mate."
-        label3.numberOfLines = 0
-        label3.textAlignment = .center
-        label3.textColor = UIColor.white
-        self.quickGuideStackView.addArrangedSubview(label)
-        self.quickGuideStackView.addArrangedSubview(label2)
-        self.quickGuideStackView.addArrangedSubview(label3)
+        return label
     }
     
 }
